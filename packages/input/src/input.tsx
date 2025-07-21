2432f5fd4e7c4bd83d2cb3eb6af2758f06fedf29
lariane-guerra <larianespalenza@gmail.com>
2025-07-21 11:02:44 -0300

    test focus and others class

--- packages/input/src/input.tsx
+++ packages/input/src/input.tsx
@@ -2,5 +2,5 @@
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 export function Input({  ...props }: InputProps) {
-  return <input className={'text-white bg-purple-400 rounded-md border-2 border-purple-100 p-2 w-2xs'} />
+  return <input className={'text-white bg-blue-300 rounded-md border-2 border-purple-100 p-2 w-2xs focus:outline-none focus:ring-1 focus:ring-blue-500'} {...props} />
 }
